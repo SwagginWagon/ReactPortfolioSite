2b764214cda0b5cd6bc77cd9a912963fe62c96bb
Dylan Wagner <dylan.wagner10@gmail.com>
2022-11-09 15:00:29 -0500

    Adjusted container code in each component

--- portfolio-site/src/components/Footer.js
+++ portfolio-site/src/components/Footer.js
@@ -3,31 +3,33 @@ import { Scrollchor } from "react-scrollchor";
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12 col-md-4">
-                        <nav>
-                            <ul className="nav justify-content-start">
-                                <li className="nav-item">
-                                    <Scrollchor className="nav-link" to="">Home</Scrollchor>
-                                </li>
-                                <li className="nav-item">
-                                    <Scrollchor className="nav-link" to="#career-section">Career</Scrollchor>
-                                </li>
-                                <li className="nav-item">
-                                    <Scrollchor className="nav-link" to="#portfolio-section">Portfolio</Scrollchor>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+        <>
+            <footer>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-12 col-md-4">
+                            <nav>
+                                <ul className="nav justify-content-start">
+                                    <li className="nav-item">
+                                        <Scrollchor className="nav-link" to="">Home</Scrollchor>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Scrollchor className="nav-link" to="#career-section">Career</Scrollchor>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Scrollchor className="nav-link" to="#portfolio-section">Portfolio</Scrollchor>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
 
-                    <div className="col-12 col-md-8 d-flex justify-content-end align-items-center">
-                        <small>Copyright © <span id="copyright-year"></span> Dylan Wagner</small>
+                        <div className="col-12 col-md-8 d-flex justify-content-end align-items-center">
+                            <small>Copyright © <span id="copyright-year"></span> Dylan Wagner</small>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </>
     );
 };
 
--- portfolio-site/src/components/PortfolioSection.js
+++ portfolio-site/src/components/PortfolioSection.js
@@ -8,31 +8,33 @@ import FCImg from "../assets/images/frankenmuthcertified.com_min.png"
 
 const PortfolioSection = () => {
     return (
-        <section id="portfolio-section">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h2 className="blue-text text-center"><b>Some Work from RJM</b></h2>
+        <>
+            <section id="portfolio-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h2 className="blue-text text-center"><b>Some Work from RJM</b></h2>
+                        </div>
+                    </div>
+
+                    <div className="row mt-4">
+                        <div className="col-12 col-md-4 d-flex flex-column">
+                            <Project url="http://rjm.marketing" image={RJMImg} alt="RJM Website" overlay="RJM" />
+                            <Project url="http://experiencejackson.com" image={EJImg} alt="Experience Jackson Website" overlay="Experience Jackson" />
+                        </div>
+
+                        <div className="col-12 col-md-4 d-flex flex-column">
+                            <Project url="http://www.aladdinjackson.com" image={AEImg} alt="Aladdin Electric Website" overlay="Aladdin Electric" />
+                            <Project url="http://www.ljross.com" image={LJRImg} alt="LJ Ross Website" overlay="LJ Ross" />
+                        </div>
+
+                        <div className="col-12 col-md-4 d-flex flex-column">
+                            <Project url="http://frankenmuthcertified.com"image={FCImg} alt="Frankenmuth Certified Website" overlay="Frankenmuth Certified" />
+                        </div>
                     </div>
                 </div>
-
-                <div className="row mt-4">
-                    <div className="col-12 col-md-4 d-flex flex-column">
-                        <Project url="http://rjm.marketing" image={RJMImg} alt="RJM Website" overlay="RJM" />
-                        <Project url="http://experiencejackson.com" image={EJImg} alt="Experience Jackson Website" overlay="Experience Jackson" />
-                    </div>
-
-                    <div className="col-12 col-md-4 d-flex flex-column">
-                        <Project url="http://www.aladdinjackson.com" image={AEImg} alt="Aladdin Electric Website" overlay="Aladdin Electric" />
-                        <Project url="http://www.ljross.com" image={LJRImg} alt="LJ Ross Website" overlay="LJ Ross" />
-                    </div>
-
-                    <div className="col-12 col-md-4 d-flex flex-column">
-                        <Project url="http://frankenmuthcertified.com"image={FCImg} alt="Frankenmuth Certified Website" overlay="Frankenmuth Certified" />
-                    </div>
-                </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
 
